9aa257311934c1ae81e1f58f77500469621bee58
Your Name <gabrielledyradiante@hotmail.com>
2021-10-02 18:24:53 -0300

    creatin menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { LoginComponent } from './main/login/login.component';
+import { SidebarComponent } from './main/sidebar/sidebar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"sign_in",component:SidebarComponent},
+  {path:"",component:LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
+import { MainModule } from './main/main.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+
+import { MatSliderModule } from '@angular/material/slider';
+
+
+ 
+
 
 @NgModule({
   declarations: [
@@ -10,8 +21,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatIconModule,    
+    MatMenuModule,   
+    MatSliderModule,    
+    MainModule
   ],
+  exports:[
+   
+    MatIconModule,    
+    MatMenuModule,    
+    MatSliderModule,   
+    MainModule
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
