444a7b9b95abf870d55a95b8fb2c1abe5fe9dad8
Rafael Faia <RafaelFaiaC@gmail.com>
2021-10-04 20:18:16 -0300

    extract creation

--- src/app/main/extract/extract.component.ts
+++ src/app/main/extract/extract.component.ts
@@ -8,16 +8,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Brasil', weight: 1.0079, symbol: 'Dolar'},
-  {position: 2, name: 'Itau', weight: 4.0026, symbol: 'Real'},
-  {position: 3, name: 'Bradesco', weight: 6.941, symbol: 'Real'},
-  {position: 4, name: 'Nubank', weight: 9.0122, symbol: 'Real'},
-  {position: 5, name: 'Nubank', weight: 10.811, symbol: 'Real'},
-  {position: 6, name: 'Nubank', weight: 12.0107, symbol: 'Real'},
-  {position: 7, name: 'Nubank', weight: 14.0067, symbol: 'Real'},
-  {position: 8, name: 'Nubank', weight: 15.9994, symbol: 'Real'},
-  {position: 9, name: 'Nubank', weight: 18.9984, symbol: 'Real'},
-  {position: 10, name: 'Nubank', weight: 20.1797, symbol: 'Real'},
+  {position: 1, name: 'PAN', weight: 1.0079, symbol: 'Dólar Americano',},
+  {position: 2, name: 'INTER', weight: 4.0026, symbol: 'Euro'},
+  {position: 3, name: 'CAIXA', weight: 6.941, symbol: 'Libra esterlina'},
+  {position: 4, name: 'SANTANDER', weight: 9.0122, symbol: 'Iene'},
+  {position: 5, name: 'BRADESCO', weight: 10.811, symbol: 'Dólar Australiano'},
+  {position: 6, name: 'BB', weight: 12.0107, symbol: 'Franco Suíço'},
+  {position: 7, name: 'ITAU', weight: 14.0067, symbol: 'Dólar Canadense'},
+  {position: 8, name: 'MIDWAY', weight: 15.9994, symbol: 'Renminbi (Yuan)'},
+  {position: 9, name: 'VOTORANTIM', weight: 18.9984, symbol: 'Peso Argentino'},
+  {position: 10, name: 'NUBANK', weight: 20.1797, symbol: 'Real'},
 ];
 
 @Component({
--- src/app/main/withdraw/withdraw.component.ts
+++ src/app/main/withdraw/withdraw.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: number ;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position:'13-01-2021' , name: 'Saldo Anterior', weight: '', symbol: 969.928},
+  {position: '16-02-2021', name: '00:09:31', weight: '4.0026', symbol: 2},
+  {position: '17-02-2021', name: '15:04:01', weight:' 6.941', symbol: 2},
+  {position: '08-03-2021', name: '18:18:25', weight: '9.0122', symbol: 2},
+  {position: '28-03-2021', name: '04:48:57', weight: '10.811', symbol: 2},
+  {position: '19-05-2021', name: '10:30:24', weight:' 12.0107', symbol: 2},
+  {position: '09-06-2021', name: '03:49:03', weight: '14.0067', symbol:2},
+  {position: '05-08-2021', name: '04:23:06', weight: '15.9994', symbol: 2},
+  {position: '07-09-2021', name: '00:51:37', weight: '18.9984', symbol:2},
+  {position: '04-10-2021', name: '18:23:07',weight: '20.1797', symbol: 2},
+];
+
 @Component({
   selector: 'app-withdraw',
   templateUrl: './withdraw.component.html',
   styleUrls: ['./withdraw.component.scss']
 })
-export class WithdrawComponent implements OnInit {
 
+export class WithdrawComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  clickedRows = new Set<PeriodicElement>();
   constructor() { }
 
   ngOnInit(): void {
