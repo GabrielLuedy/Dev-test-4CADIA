a995b178a9d31ec9000c04a08c78c78a03236a96
Rafael Faia <RafaelFaiaC@gmail.com>
2021-10-05 15:37:30 -0300

    data formatting

--- src/app/main/extract/extract.component.ts
+++ src/app/main/extract/extract.component.ts
@@ -3,21 +3,23 @@ import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
+  weight: string;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'PAN', weight: 1.0079, symbol: 'Dólar Americano',},
-  {position: 2, name: 'INTER', weight: 4.0026, symbol: 'Euro'},
-  {position: 3, name: 'CAIXA', weight: 6.941, symbol: 'Libra esterlina'},
-  {position: 4, name: 'SANTANDER', weight: 9.0122, symbol: 'Iene'},
-  {position: 5, name: 'BRADESCO', weight: 10.811, symbol: 'Dólar Australiano'},
-  {position: 6, name: 'BB', weight: 12.0107, symbol: 'Franco Suíço'},
-  {position: 7, name: 'ITAU', weight: 14.0067, symbol: 'Dólar Canadense'},
-  {position: 8, name: 'MIDWAY', weight: 15.9994, symbol: 'Renminbi (Yuan)'},
-  {position: 9, name: 'VOTORANTIM', weight: 18.9984, symbol: 'Peso Argentino'},
-  {position: 10, name: 'NUBANK', weight: 20.1797, symbol: 'Real'},
+  {position: 1, name: 'PAN', weight: '+ R$ 1.0079', symbol: 'Dólar Americano',},
+  {position: 2, name: 'INTER', weight: '+ R$ 4.0026', symbol: 'Euro'},
+  {position: 3, name: 'CAIXA', weight: '+ R$ 6.941', symbol: 'Libra esterlina'},
+  {position: 4, name: 'SANTANDER', weight: '+ R$ 9.0122', symbol: 'Iene'},
+  {position: 5, name: 'BRADESCO', weight: '+ R$ 10.811', symbol: 'Dólar Australiano'},
+  {position: 6, name: 'BB', weight: '+ R$ 12.0107', symbol: 'Franco Suíço'},
+  {position: 7, name: 'ITAU', weight: '+ R$ 14.0067', symbol: 'Dólar Canadense'},
+  {position: 8, name: 'MIDWAY', weight:'+ R$  15.9994', symbol: 'Renminbi (Yuan)'},
+  {position: 9, name: 'VOTORANTIM', weight: '+ R$ 18.9984', symbol: 'Peso Argentino'},
+  {position: 10, name: 'NUBANK', weight: '+ R$ 20.1797', symbol: 'Real'},
+  {position: 11, name: 'INVESTMENT BALANC', symbol: '', weight: ' R$ 969.928'},
+
 ];
 
 @Component({
--- src/app/main/withdraw/withdraw.component.ts
+++ src/app/main/withdraw/withdraw.component.ts
@@ -10,28 +10,28 @@ export interface PeriodicElement {
 const ELEMENT_DATA: PeriodicElement[] = [
   {position:'13-01-2021' , name: 'SALDO ANTERIOR', weight: '', symbol: 969.928},
 
-  {position: '16-02-2021', name: 'Transf. Eletr. Banc. INTER', weight: '- 4.0026', symbol: 929.902},
-  {position: '17-02-2021', name: 'Transf. Eletr. Banc. CAIXA', weight:' - 6.941', symbol: 922.961},
+  {position: '16-02-2021', name: 'Transf. Eletr. Banc. INTER', weight: '- R$4.0026', symbol: 929.902},
+  {position: '17-02-2021', name: 'Transf. Eletr. Banc. CAIXA', weight:' - R$6.941', symbol: 922.961},
 
   {position:'01-03-2021' , name: 'SALDO CONTA ', weight: '', symbol: 922.961},
 
-  {position: '08-03-2021', name: 'Transf. Eletr. Banc. SANTANDER', weight: '- 9.0122', symbol:832.839},
-  {position: '28-03-2021', name: 'Transf. Eletr. Banc. BRADESCO', weight: '- 10.811', symbol:822.028 },
+  {position: '08-03-2021', name: 'Transf. Eletr. Banc. SANTANDER', weight: '- R$9.0122', symbol:832.839},
+  {position: '28-03-2021', name: 'Transf. Eletr. Banc. BRADESCO', weight: '- R$10.811', symbol:822.028 },
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 822.028},
 
-  {position: '19-05-2021', name: 'Transf. Eletr. Banc. BB', weight:' - 12.0107', symbol: 701.921},
-  {position: '09-06-2021', name: 'Transf. Eletr. Banc. ITAU', weight: '- 14.0067', symbol:561.854},
+  {position: '19-05-2021', name: 'Transf. Eletr. Banc. BB', weight:' - R$12.0107', symbol: 701.921},
+  {position: '09-06-2021', name: 'Transf. Eletr. Banc. ITAU', weight: '- R$14.0067', symbol:561.854},
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 561.854},
 
-  {position: '05-08-2021', name: 'Transf. Eletr. Banc. MIDWAY', weight: '- 15.9994', symbol:401.860},
-  {position: '07-09-2021', name: 'Transf. Eletr. Banc. VOTORANTIM', weight: '- 18.9984', symbol:211.876},
+  {position: '05-08-2021', name: 'Transf. Eletr. Banc. MIDWAY', weight: '- R$15.9994', symbol:401.860},
+  {position: '07-09-2021', name: 'Transf. Eletr. Banc. VOTORANTIM', weight: '- R$18.9984', symbol:211.876},
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 211.876},
 
-  {position: '04-10-2021', name: 'Transf. Eletr. Banc. NUBANK',weight: '- 20.1797', symbol: 10.079},
-  {position: '04-10-2021', name: 'Transf. Eletr. Banc. PAN',weight: '- 10.079', symbol: 0},
+  {position: '04-10-2021', name: 'Transf. Eletr. Banc. NUBANK',weight: '- R$20.1797', symbol: 10.079},
+  {position: '04-10-2021', name: 'Transf. Eletr. Banc. PAN',weight: '- R$10.079', symbol: 0},
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 0},
 
