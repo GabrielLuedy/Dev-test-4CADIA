25070e8a0e9201a44b77fda8fa027c1d4f313ecc
Your Name <gabrielledyradiante@hotmail.com>
2021-10-05 00:02:41 -0300

    output option creation

--- src/app/main/withdraw/withdraw.component.ts
+++ src/app/main/withdraw/withdraw.component.ts
@@ -20,18 +20,18 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 822.028},
 
-  {position: '19-05-2021', name: 'Transf. Eletr. Banc. BB', weight:' 12.0107', symbol: 701.921},
-  {position: '09-06-2021', name: 'Transf. Eletr. Banc. ITAU', weight: '14.0067', symbol:561.854},
+  {position: '19-05-2021', name: 'Transf. Eletr. Banc. BB', weight:' - 12.0107', symbol: 701.921},
+  {position: '09-06-2021', name: 'Transf. Eletr. Banc. ITAU', weight: '- 14.0067', symbol:561.854},
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 561.854},
 
-  {position: '05-08-2021', name: 'Transf. Eletr. Banc. MIDWAY', weight: '15.9994', symbol:401.860},
-  {position: '07-09-2021', name: 'Transf. Eletr. Banc. VOTORANTIM', weight: '18.9984', symbol:211.876},
+  {position: '05-08-2021', name: 'Transf. Eletr. Banc. MIDWAY', weight: '- 15.9994', symbol:401.860},
+  {position: '07-09-2021', name: 'Transf. Eletr. Banc. VOTORANTIM', weight: '- 18.9984', symbol:211.876},
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 211.876},
 
-  {position: '04-10-2021', name: 'Transf. Eletr. Banc. NUBANK',weight: '20.1797', symbol: 10.079},
-  {position: '04-10-2021', name: 'Transf. Eletr. Banc. PAN',weight: '10.079', symbol: 0},
+  {position: '04-10-2021', name: 'Transf. Eletr. Banc. NUBANK',weight: '- 20.1797', symbol: 10.079},
+  {position: '04-10-2021', name: 'Transf. Eletr. Banc. PAN',weight: '- 10.079', symbol: 0},
 
   {position:'01-04-2021' , name: 'SALDO CONTA ', weight: '', symbol: 0},
 
